268968220f4d51035b10895d242d1ef1fbde8bfd
chadabhavani23 <72121613+chadabhavani23@users.noreply.github.com>
2026-09-16 22:29:54 +0530

    Initial Commit

---
+++ ExpenseViewer/ExpenseViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseViewerApp.swift
+//  ExpenseViewer
+//
+//  Created by chada bhavani on 16/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
